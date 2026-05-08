9a5a6e18bcb1863b300b7b0d209f5efa44b4abfb
Eliaselcrack777acuna <eliasacunaet32@gmail.com>
2026-05-08 13:52:56 -0300

    Add files via upload

---
+++ tp_n_21.ino
@@ -0,0 +1,69 @@
+int potR = A0;
+int potG = A1;
+int potB = A2;
+
+int ledR = 11;
+int ledG = 10;
+int ledB = 9;
+
+int boton = 2;
+
+bool enProceso = false;
+
+void setup() {
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledB, OUTPUT);
+  pinMode(boton, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int lecturaTemp = analogRead(sensorTemp);
+  int lecturaLuz = analogRead(sensorLuz);
+
+  float voltaje = lecturaTemp * 5.0 / 1023.0;
+  float temperatura = (voltaje - 0.5) * 100.0;
+
+  int porcentajeLuz = map(lecturaLuz, 0, 1023, 0, 100);
+
+  Serial.print("El nivel de luz actual es: ");
+  Serial.print(porcentajeLuz);
+  Serial.print("% y la temperatura actual: ");
+  Serial.print(temperatura);
+  Serial.println(" °C");
+
+  digitalWrite(ledR, LOW);
+  digitalWrite(ledG, LOW);
+  digitalWrite(ledB, LOW);
+    while (millis() - inicio < 10000){
+
+      valorR = analogRead(potR);
+      valorG = analogRead(potG);
+      valorB = analogRead(potB);
+
+      valorR = map(valorR, 0, 1023, 0, 255);
+      valorG = map(valorG, 0, 1023, 0, 255);
+      valorB = map(valorB, 0, 1023, 0, 255);
+
+      analogWrite(ledR, valorR);
+      analogWrite(ledG, valorG);
+      analogWrite(ledB, valorB);
+    }
+
+    Serial.print("El led esta usando esta configuracion de colores RGB: (");
+    Serial.print(valorR);
+    Serial.print(", ");
+    Serial.print(valorG);
+    Serial.print(", ");
+    Serial.print(valorB);
+    Serial.println(")");
+    
+    analogWrite(ledR, 0);
+    analogWrite(ledG, 0);
+    analogWrite(ledB, 0);
+
+    enProceso = false;
+  }
+}
